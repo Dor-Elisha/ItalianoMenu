b22a526d2cc08c171963122dc5f401d27a168ab0
Dor-Elisha <dorelisha21@gmail.com>
2026-04-15 15:47:48 +0300

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/ItalianoMenu/", // Replace with your GitHub username/repo-name
   resolve: {
     alias: {
       "@": new URL("./src", import.meta.url).pathname,
